d13d1197687f03e0bf4fa4496de2e7d3bbd77169
SeedPaths-User <lessdismissive@email.com>
2014-04-25 13:48:53 -0600

    V 1.0

---
+++ ObjectPractice/ObjectPractice/Food.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Food
+    {
+        public bool chewy { get; set; }
+        public bool hard { get; set; }
+        public bool spicy { get; set; }
+        public bool sweet { get; set; }
+        public bool liquid { get; set; }
+        public bool solid { get; set; }
+
+        public string eatFood(string foodType)
+        {
+            this.lowersHunger = 10;
+        }
+    }
+}
